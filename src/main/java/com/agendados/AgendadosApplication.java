ecfa7f3a97682a9b03c5a8a9d5fe9a8039f66495
Lautaro Montaña <joselautarom@gmail.com>
2025-03-21 08:33:52 -0300

    Primer commit

---
+++ src/main/java/com/agendados/AgendadosApplication.java
@@ -0,0 +1,13 @@
+package com.agendados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendados/AgendadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
