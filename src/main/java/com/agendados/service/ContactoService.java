493350ccec17bdf6799585cfdd24560962c261af
Lautaro Montaña <joselautarom@gmail.com>
2025-05-18 19:26:15 -0300

    Arreglo de bug - ContactoService

--- src/main/java/com/agendados/service/ContactoService.java
+++ src/main/java/com/agendados/service/ContactoService.java
@@ -2,47 +2,48 @@ package com.agendados.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import com.agendados.model.Contacto;
+
 import org.springframework.stereotype.Service;
+
+import com.agendados.model.Contacto;
 
 @Service
 public class ContactoService {
 
-    private Map<String, Contacto> agenda = new HashMap<>();
+    private final Map<String, Contacto> agenda = new HashMap<>();
 
-    public Map<String, Contacto> obtenerContacto(){
+    public Map<String, Contacto> obtenerContacto() {
         return agenda;
     }
 
-    public boolean eliminarContacto(String nombre){
+    public boolean eliminarContacto(String nombre) {
         Contacto contacto = agenda.get(nombre);
-        if(contacto != null && !contacto.isFavorito()){
+        if (contacto != null && !contacto.isFavorito()) {
             agenda.remove(nombre);
             return true;
         }
         return false;
     }
 
-    public Contacto obtenerContactoPorNombre(String nombre){
+    public Contacto obtenerContactoPorNombre(String nombre) {
         return agenda.get(nombre);
     }
 
-    public void actualizarContacto(String nombreOriginal, Contacto contactoActualizado){
-        if(!nombreOriginal.equals(contactoActualizado.getNombre())){
+    public void actualizarContacto(String nombreOriginal, Contacto contactoActualizado) {
+        if (!nombreOriginal.equals(contactoActualizado.getNombre())) {
             agenda.remove(nombreOriginal);
         }
         agenda.put(contactoActualizado.getNombre(), contactoActualizado);
     }
 
-    public void cambiarFavorito(String nombre){
+    public void cambiarFavorito(String nombre) {
         Contacto contacto = agenda.get(nombre);
-        if(contacto != null){
+        if (contacto != null) {
             contacto.setFavorito(!contacto.isFavorito());
         }
     }
 
-    public void guardarContacto(Contacto contacto){
+    public void guardarContacto(Contacto contacto) {
         agenda.put(contacto.getNombre(), contacto);
     }
-
 }
